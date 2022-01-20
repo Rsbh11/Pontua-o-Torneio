42c7854400aa3cc156f77aa9f693dbfc55755d13
Rsbh11 <94760557+Rsbh11@users.noreply.github.com>
2022-01-20 14:15:08 -0300

    Add files via upload

---
+++ Pontuação.py
@@ -0,0 +1,15 @@
+print("Nosso time Fez:")
+
+p1=int(4+0)
+p2=int(2+0)
+p3=int(2+0)
+p4=int(3-2)
+p5=int(3-1)
+p6=int(4+0)
+p7=int(2+0)
+p8=int(2-1)
+p9=int(3-2)
+p10=int(3-1)
+
+Resultado=int(p1+p2+p3+p4+p5+p6+p7+p8+p9+p10)
+print(Resultado, "Pontos")
